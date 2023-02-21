96d8686809ce9f4beff7f50445c625360e93b1d6
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-02-21 18:31:54 -0300

    criação- efeito span/h1/h2- box_text - entrada

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
+// *************EFEITO NAV_MENU BOX_TOP*************
+
 var boxTop = document.querySelector('div.box_top');
-var navMenu = documente.querySelector('nav.menu_desktop a:hover');
 
-
-function hoverAm(navMenu){
+function hoverAm(){
     
     boxTop.style.width = "41.8%";
     
 }
 
-function hoverHabil(navMenu){
+function hoverHabil(){
     
     boxTop.style.width = "56.5%";
     
 }
 
-function hoverPort(navMenu){
+function hoverPort(){
     
     boxTop.style.width = "69.05%";
     
 }
 
-function hoverConta(navMenu){
+function hoverConta(){
     
     boxTop.style.width = "82.19%";
     
@@ -30,3 +30,38 @@ boxTop.onmouseover = hoverAm;
 boxTop.onmouseover = hoverHabil;
 boxTop.onmouseover = hoverPort;
 boxTop.onmouseover = hoverConta;
+
+
+// *************FIM NAV_MENU BOX_TOP*************
+
+
+function typeWriter(elemento){
+    const textEfect = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textEfect.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 65 * i);
+    });
+}
+
+    const titulo = document.querySelector('span');
+    typeWriter(titulo);
+
+
+
+    function typeWritertwo(elementos){
+        const textEfect = elementos.innerHTML.split('');
+        elementos.innerHTML = '';
+        textEfect.forEach((letra, i) => {
+            setTimeout(() => elementos.innerHTML += letra, 105 * i);
+        });
+    }
+    
+        const subtitulo = document.querySelector('h2');
+        typeWritertwo(subtitulo);
+
+    
+        
+        
+
+    
+    
